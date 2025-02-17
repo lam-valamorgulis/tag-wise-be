cc67e993ee0ff0bf93237487d1552a4bbf2172bf
valamorgulis <lamdang0306@gmail.com>
2025-02-17 08:57:41 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: './src/.env' });
 const app = require('./app');
 const { mongoConnect } = require('./service/mongo');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000; // Set your desired port here
 
 const server = http.createServer(app);
 
@@ -13,6 +13,7 @@ async function startServer() {
   try {
     await mongoConnect();
     server.listen(PORT, '0.0.0.0', () => {
+      // Bind to 0.0.0.0 to accept external connections
       console.log(`Listening on port ${PORT}...`);
     });
   } catch (err) {
